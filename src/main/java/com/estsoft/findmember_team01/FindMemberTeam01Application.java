a9e3d68aa43a54281291e833937d133a816527e8
rma4921 <rma4921@naver.com>
2025-04-24 14:01:55 +0900

    Initial commit on init branch

---
+++ src/main/java/com/estsoft/findmember_team01/FindMemberTeam01Application.java
@@ -0,0 +1,13 @@
+package com.estsoft.findmember_team01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMemberTeam01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMemberTeam01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/estsoft/findmember_team01/FindMemberTeam01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.findmember_team01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMemberTeam01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
