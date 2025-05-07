845c5e59a6e8458a0e0388eba08a72f43a403a95
NohYeongJae <nyj1665@naver.com>
2025-05-07 17:25:38 +0900

    #   refactor    : 불필요한 코드 삭제

--- src/main/java/com/estsoft/findmember_team01/information/service/InformationService.java
+++ src/main/java/com/estsoft/findmember_team01/information/service/InformationService.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
