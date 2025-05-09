978283e4dddab8c836925120a6eaffe4c6b2e989
hy <hyeyunn01@naver.com>
2025-05-09 16:22:18 +0900

    Feat: 회원 등급 변경 기능 추가

--- src/main/java/com/estsoft/findmember_team01/admin/controller/AdminController.java
+++ src/main/java/com/estsoft/findmember_team01/admin/controller/AdminController.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,5 +73,13 @@ public class AdminController {
         @RequestParam UpdateLevelRequest request) {
         adminService.updateUserLevel(id, request.getLevel());
         return ResponseEntity.ok("레벨 및 권한이 수정되었습니다.");
+    }
+
+    @PostMapping("/api/admin/users/{id}/role")
+    public String changeMemberRole(@PathVariable Long id,
+        @RequestParam String role) {
+        adminService.changeRole(id, role);
+
+        return "redirect:/api/admin/users";
     }
 }
