4461a92d73545c21becdd7d507994d0017411e06
rma4921 <rma4921@naver.com>
2025-05-09 10:37:17 +0900

    Fix(login): 로그인시 이전 페이지 접근 불가 오류 해결 - SavedRequest 대신 이전 페이지에 대한 세션 정보 저장값으로 처리하도록 수정 - 그에 따른 조건문 수정

--- src/main/java/com/estsoft/findmember_team01/member/handler/UserAuthenticationSuccessHandler.java
+++ src/main/java/com/estsoft/findmember_team01/member/handler/UserAuthenticationSuccessHandler.java
@@ -9,38 +9,38 @@ import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-	private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-	// 인증 핸들러
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-		Authentication authentication) throws IOException, ServletException {
-		String email = authentication.getName();
-		String userAgent = request.getHeader("User-Agent");
+    // 인증 핸들러
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
+        String email = authentication.getName();
+        String userAgent = request.getHeader("User-Agent");
 
-		Member member = memberRepository.findByEmail(email)
-			.orElseThrow(() -> new IllegalArgumentException(email + "에 해당하는 사용자 정보를 찾을 수 없습니다."));
-		member.updateLoginInfo(userAgent);
-		memberRepository.save(member);
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new IllegalArgumentException(email + "에 해당하는 사용자 정보를 찾을 수 없습니다."));
+        member.updateLoginInfo(userAgent);
+        memberRepository.save(member);
 
-		request.getSession().setAttribute("memberId", member.getId());
-		request.getSession().setAttribute("memberLevel", member.getLevel());
-		request.getSession().setAttribute("memberNickname", member.getNickname());
+        request.getSession().setAttribute("memberId", member.getId());
+        request.getSession().setAttribute("memberLevel", member.getLevel());
+        request.getSession().setAttribute("memberNickname", member.getNickname());
 
         boolean isAdmin = authentication.getAuthorities().stream()
             .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
 
-        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
-        if (savedRequest != null) {
-            response.sendRedirect(savedRequest.getRedirectUrl());
+        String redirectUrl = (String) request.getSession().getAttribute("prevPage");
+        request.getSession().removeAttribute("prevPage");
+
+        if (redirectUrl != null) {
+            response.sendRedirect(redirectUrl);
         } else {
             response.sendRedirect(isAdmin ? "/api/admin/users" : "/api/posts");
         }
