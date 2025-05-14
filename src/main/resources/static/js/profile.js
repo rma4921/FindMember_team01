86a3f1ec6542da567e2d647f19eb47626423720a
rma4921 <rma4921@naver.com>
2025-05-14 16:38:04 +0900

    Fix(profile): 관리자도 이미지 등록할 수 있도록 추가

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -11,7 +11,7 @@ function submitForm() {
 }
 
 function onImageButtonClick(level) {
-    if (level < 5) {
+    if (level < 5 && level != 0) {
         alert('레벨 5부터 대표 이미지를 설정할 수 있습니다.');
     } else {
         document.getElementById('imageUpload').click();
